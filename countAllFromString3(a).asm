b863287529875d22af4b152480b71d2c0d0f6793
mahadee hasan abir <37393027+mahade10@users.noreply.github.com>
2020-02-24 20:44:37 +0600

    Update countAllFromString3(a).asm

--- countAllFromString3(a).asm
+++ countAllFromString3(a).asm
@@ -1,145 +1,169 @@
-
 INCLUDE 'EMU8086.INC'
 .MODEL SMALL
 .STACK 100H
 .DATA
 
 VL DB 0
-CN DB 0
-NO DB 0
-NU DB 0
-OT DB 0
+CNS DB 0
+NUMBER DB 0
+SPACE DB 0
+OTHER DB 0
 
-.CODE      
-MOV AX, @DATA
-MOV DS, AX
+.CODE
+
+MOV AX,@DATA
+MOV DS,AX 
 
 MAIN PROC
-    MOV VL, 0
-    MOV CN, 0
-    MOV NO, 0
-    MOV NU, 0
-    MOV OT, 0
-    MOV AH, 1
-    FOR: INT 21H
-        CMP AL, 0DH
-        JE RESULT
-        CMP AL, 'A'
+    
+    MOV VL,0
+    MOV CNS,0
+    MOV NUMBER,0
+    MOV SPACE,0
+    MOV OTHER,0
+    
+    MOV AH,1
+    NEXT:
+        INT 21H
+        CMP AL,0DH
+        JZ OUTPUT
+        
+        CMP AL,'A'
         JL OTHERS
         CMP AL, 'z'
         JG OTHERS
-        JMP INTER 
         
-    CHAR: CMP AL, 'A'
-        JE VOWEL
-        CMP AL, 'a'
-        JE VOWEL
-        CMP AL, 'E'
-        JE VOWEL
-        CMP AL, 'e'
-        JE VOWEL
-        CMP AL, 'I'
-        JE VOWEL
-        CMP AL, 'i'
-        JE VOWEL
-        CMP AL, 'O'
-        JE VOWEL
-        CMP AL, 'o'
-        JE VOWEL
-        CMP AL, 'U'
-        JE VOWEL
-        CMP AL, 'u'
-        JE VOWEL
+        JMP CHAR
         
-        JMP CONSONENT
-    
-    VOWEL: INC VL
-        JMP FOR
+   CHAR:
+        CMP AL,'A'
+        JZ VOWEL
         
-    CONSONENT: INC CN
-        JMP FOR
+        CMP AL,'E'
+        JZ VOWEL
         
-    OTHERS: CMP AL, ' '
-        JNE SPECIAL
-        INC NU
-        JMP FOR
+        CMP AL,'I'
+        JZ VOWEL
         
-    SPECIAL: CMP AL, '0'
-        JL ALL
-        CMP AL, '9'
-        JG ALL
-        INC NO
-        JMP FOR
+        CMP AL,'O'
+        JZ VOWEL
         
-    INTER: CMP AL, 'Z'
-        JL CHAR
-        CMP AL, 'a'
-        JG CHAR
+        CMP AL,'U'
+        JZ VOWEL
         
-    ALL: INC OT
-        JMP FOR 
-    
-    RESULT:
-    
-        MOV AH, 2
-        MOV DL, 0AH
-        INT 21H
-        MOV DL, 0DH
-        INT 21H
-    
-        ADD VL, 48
-        PRINTN
-        PRINT 'VOWEL: '
-        MOV DL, VL 
-        INT 21H
+        CMP AL,'a'
+        JZ VOWEL
         
-        MOV DL, 0AH
-        INT 21H
-        MOV DL, 0DH
-        INT 21H
+        CMP AL,'e'
+        JZ VOWEL
         
-        ADD CN, 48
-        PRINTN
-        PRINT 'CONSONENT: '
-        MOV DL, CN 
-        INT 21H
+        CMP AL,'i'
+        JZ VOWEL
         
-        MOV DL, 0AH
-        INT 21H
-        MOV DL, 0DH
-        INT 21H
+        CMP AL,'o'
+        JZ VOWEL
         
+        CMP AL,'u'
+        JZ VOWEL
         
-         
-        ADD NO, 48
-        PRINTN
-        PRINT 'DIGIT: '
-        MOV DL, NO 
-        INT 21H
+        JMP CONST
         
-        MOV DL, 0AH
-        INT 21H
-        MOV DL, 0DH
-        INT 21H
+    VOWEL:
+          INC VL
+          JMP NEXT
+          
+    CONST:
+          INC CNS
+          JMP NEXT
+          
+    OTHERS:
+           CMP AL,' '
+           JNE SPECIAL
+           INC SPACE
+           JMP NEXT
+           
+    SPECIAL:
+            CMP AL,'0'
+            JL ALL
+            CMP AL,'9'
+            JG ALL
+            INC NUMBER
+            JMP NEXT
+            
+     ALL:
+            INC OTHER
+            JMP NEXT
+            
+     OUTPUT:
+            MOV AH,2
+            MOV DL,0AH
+            INT 21H
+            MOV DL,0DH
+            INT 21H
+            
+            ADD VL,48
+            PRINTN
+            PRINT 'VOWEL: '
+            MOV DL,VL
+            INT 21H
+           
+            MOV DL, 0AH
+            INT 21H
+            MOV DL, 0DH
+            INT 21H
+            
+            
+            ADD CNS,48
+            PRINTN
+            PRINT 'CONSONENT: '
+            MOV DL,CNS
+            INT 21H
+            
+            MOV DL, 0AH
+            INT 21H
+            MOV DL, 0DH
+            INT 21H
+            
+            ADD NUMBER,48
+            PRINTN
+            PRINT 'NUMBERS: '
+            MOV DL,NUMBER
+            INT 21H
+            
+            MOV DL, 0AH
+            INT 21H
+            MOV DL, 0DH
+            INT 21H
+            
+            ADD SPACE,48
+            PRINTN
+            PRINT 'SPACES: '
+            MOV DL,SPACE
+            INT 21H
+            
+            MOV DL, 0AH
+            INT 21H
+            MOV DL, 0DH
+            INT 21H
+            
+            ADD OTHER,48
+            PRINTN
+            PRINT 'OTHERS: '
+            MOV DL,OTHER
+            INT 21H
+            
+            MOV DL, 0AH
+            INT 21H
+            MOV DL, 0DH
+            INT 21H
+            
+            MOV AH,4CH
+            INT 21H
+            
+     MAIN ENDP
+     END MAIN
+            
+            
+                                             
         
-        ADD NU, 48
-        PRINTN
-        PRINT 'SPACE: '
-        MOV DL, NU 
-        INT 21H
-        
-        MOV DL, 0AH
-        INT 21H
-        MOV DL, 0DH
-        INT 21H
-        
-        ADD OT, 48
-        PRINTN
-        PRINT 'OTHERS: '
-        MOV DL, OT 
-        INT 21H
-        
-        MOV AH, 4CH
-        INT 21H
-MAIN ENDP
-END MAIN
+            
